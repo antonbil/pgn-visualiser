244a056b7589908422d73e8b80cd63b52e2183d2
anton <anton.bil@hz.nl>
2026-01-01 14:40:04 +0100

    comment white

--- visualise-pgn.py
+++ visualise-pgn.py
@@ -241,8 +241,8 @@ def _format_pgn_history(move_list):
                 '',
                 move['comment']
             ).strip()
-            if player == chess.WHITE:
-                clean_comment = "{1}"
+            if player == chess.WHITE and len(clean_comment) > 6:
+                clean_comment = "{{1}}"
 
             if clean_comment:
                 current_line += f" {{{clean_comment}}}"
