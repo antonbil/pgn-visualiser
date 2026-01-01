3496d8a9cfc41531dbfba26487099ca87355a7ca
anton <anton.bil@hz.nl>
2026-01-01 15:44:45 +0100

    comment white

--- visualise-pgn.py
+++ visualise-pgn.py
@@ -240,13 +240,13 @@ def _format_pgn_history(move_list):
                 r'\s*(?<![A-Za-z])([#]?[-+]?\d+\.?\d*)(?:/\d+)?\s*|\[%eval\s*([#]?[-]?\d+\.?\d*)\]|\s*\([^\)]*\)',
                 '',
                 move['comment']
-            ).strip()
+            ).strip().replace(",","")
             if player == chess.WHITE and len(clean_comment) > 6:
                 current_line += " {1}"
             elif clean_comment and len(clean_comment) < 6:
                 current_line += f" {{{clean_comment}}}"
             elif clean_comment:
-                current_line += f" \n{{{clean_comment.replace(',','')}}}"
+                current_line += f" \n{{{clean_comment}}}"
 
     if not previous_variation is None:
         for i, variation in enumerate(previous_variation):
